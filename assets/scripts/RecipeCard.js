5dfa2ebe5fa5e2e21b310586e1009dfb1864ad19
Abby <akoornwi@ucsd.edu>
2022-11-03 19:16:38 -0700

    completed expose

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -5,12 +5,98 @@ class RecipeCard extends HTMLElement {
   // the element is written into the DOM directly as <recipe-card>
   constructor() {
     super(); // Inheret everything from HTMLElement
+    
+    let shadowEl = this.attachShadow({mode: 'open'});  // A1: create a shadow root
+    
+    let article = document.createElement('article') // A2: create article element
+    
+    let style = document.createElement('style');  //A3: create a style element
+    style.textContent = ` //A4: copy and pasted styles from cardTemplate.html
+    * {
+    font-family: sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  article {
+    align-items: center;
+    border: 1px solid rgb(223, 225, 229);
+    border-radius: 8px;
+    display: grid;
+    grid-template-rows: 118px 56px 14px 18px 15px 36px;
+    height: auto;
+    row-gap: 5px;
+    padding: 0 16px 16px 16px;
+    width: 178px;
+  }
+
+  div.rating {
+    align-items: center;
+    column-gap: 5px;
+    display: flex;
+  }
+
+  div.rating>img {
+    height: auto;
+    display: inline-block;
+    object-fit: scale-down;
+    width: 78px;
+  }
+
+  article>img {
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+    height: 118px;
+    object-fit: cover;
+    margin-left: -16px;
+    width: calc(100% + 32px);
+  }
+
+  p.ingredients {
+    height: 32px;
+    line-height: 16px;
+    padding-top: 4px;
+    overflow: hidden;
+  }
+
+  p.organization {
+    color: black !important;
+  }
+
+  p.title {
+    display: -webkit-box;
+    font-size: 16px;
+    height: 36px;
+    line-height: 18px;
+    overflow: hidden;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+  }
+
+  p:not(.title),
+  span,
+  time {
+    color: #70757A;
+    font-size: 12px;
+  }`;
+
+  // A5: append the style and article elements
+  shadowEl.append(style);
+  shadowEl.append(article);
 
     // EXPOSE - START (All expose numbers start with A)
-    // A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
-    // A2. TODO - Create an <article> element - This will hold our markup once our data is set
-    // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
-    // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
+    // A1. X-TODO - Attach the shadow DOM to this Web Component (leave the mode open)
+    // A2. ?-TODO - Create an <article> element - This will hold our markup once our data is set
+    // A3. X-TODO - Create a style element - This will hold all of the styles for the Web Component
+    // A4. X-TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
     // A5. TODO - Append the <style> and <article> elements to the Shadow DOM
   }
 
@@ -38,7 +124,25 @@ class RecipeCard extends HTMLElement {
   set data(data) {
     // If nothing was passed in, return
     if (!data) return;
+    let articlevar = this.shadowRoot.querySelector('article');  //A6: select the article
 
+    //A7: set contents of the article
+    articlevar.innerHTML = `
+    <img src="${data.imgSrc}"
+      alt="${data.imgAlt}">
+    <p class="title">
+      <a href="${data.titleLnk}">${data.titleTxt}</a>
+    </p>
+    <p class="organization">${data.organization}</p>
+    <div class="rating">
+      <span>${data.rating}</span>
+      <img src="/assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">
+      <span>(500)</span>
+    </div>
+    <time>${data.lengthTime}</time>
+    <p class="ingredients">
+      ${data.ingrediants}
+    </p>`;
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
     // A7. TODO - Set the contents of the <article> with the <article> template given in
     //           cardTemplate.html and the data passed in (You should only have one <article>,
@@ -49,3 +153,4 @@ class RecipeCard extends HTMLElement {
 
 // A8. TODO - Define the Class as a customElement so that you can create
 //           'recipe-card' elements
+customElements.define('recipe-card', RecipeCard);
